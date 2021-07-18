46ae37826eb7734ef65408cbf1a4034142b2a4c8
Himanshu Rai <raihimanshu22345gmail.com>
2021-07-19 00:04:24 +0530

    images added

--- src/components/Database.js
+++ src/components/Database.js
@@ -1,52 +1,72 @@
+import agrabg from "../images/agra/agrabg.jpg";
+import tajMahal from "../images/agra/Taj-mahal.jpg";
+import fatahpur from "../images/agra/Fatahpur.jpg";
+import agraFort from "../images/agra/Agra-fort.jpg";
+import kashmirbg from "../images/Kashmir/Kashmirbg.jpg";
+import Shrinagar from "../images/Kashmir/Shrinagar.jpg";
+import Gulmarg from "../images/Kashmir/Gulmarg.jpg";
+import Pahalgam from "../images/Kashmir/Pahalgam.jpg";
+
 const north = [
-	{
-		place: "New Delhi, NCR",
-		dp:
-			"https://www.fabhotels.com/blog/wp-content/uploads/2018/09/Red-Fort.jpg",
-		desc:
-			"New Delhi, the national capital of India, is a traveler’s paradise, to say the least. As a city that has seen the rise and fall of many dynasties, Delhi boasts of several monuments and heritage places that pay tribute to its rich cultural and political past. While there is a good range of historical places to visit in Delhi, the city doesn’t lack in gardens, museums, places of worship, adventure spots, and theme parks, either. Add to that the vibrant shopping destinations, local markets, and street food, Delhi easily tops the list of best tourist places in North India.",
-	},
-	{
-		place: "Shimla, Himachal Pradesh",
-		dp: "https://www.fabhotels.com/blog/wp-content/uploads/2019/04/Shimla1.jpg",
-		desc:
-			"Shimla, the picturesque capital city of Himachal Pradesh, is located at the foothills of the Himalayas and offers a unique travel experience with its snow-capped mountains, forests, lush green valleys, placid lakes, and pleasant climatic conditions. Apart from these natural attractions, the region also boasts of several man-made wonders including religious places, historical structures, museums, and the lively Mall Road.",
-	},
-	{
-		place: "Agra, Uttar Pradesh",
-		dp:
-			"https://www.fabhotels.com/blog/wp-content/uploads/2019/03/Taj-mahal.jpg",
-		desc:
-			"Agra, home to the famous Taj Mahal, counts among the top places to visit in North India and for all the right reasons. The historical city in Uttar Pradesh is a medley of grand monuments, exceptional architecture, and mesmerizing sights and sounds. It boasts of three UNESCO World Heritage sites, several enticing gardens, vibrant local markets, emporiums, and lip-smacking street food. Agra is also part of the famous Golden-Triangle circuit",
-	},
-	{
-		place: "Mussoorie and Dehradun, Uttarakhand",
-		dp:
-			"https://www.fabhotels.com/blog/wp-content/uploads/2019/09/Mussoorie1.jpg",
-		desc:
-			"The tree-covered hills and valleys of Dehradun and Mussoorie have attracted families and couples since the 1800s. Offering stunning views of Dehradun on one side and snow-capped Himalayan peaks on the other, the British rightly called Mussoorie, The Queen of Hill Stations. Located merely 33 km apart, these hills are also popular with trekkers. In addition to their natural beauty, Dehradun and Mussoorie boast of a number of institutions, churches, and summer palaces from the British-raj era.",
-	},
-	{
-		place: "Dharamsala and McLeodganj, Himachal Pradesh",
-		dp:
-			"https://www.fabhotels.com/blog/wp-content/uploads/2019/09/Dharamshala.jpg",
-		desc:
-			"Dharmshala and McLeodganj – less than 10 km apart – are best known as the home of the Tibetan Spiritual Leader, the Dalai Lama. Given the Tibetan influence, the calm and scenic McLeodganj is also known as Little Lhasa. Along with their natural beauty, these hill stations in North India are also known for quaint monasteries, Tibetan markets, old bungalows, and churches. They offer a sneak-peek into Tibetan culture and cuisine. Dharamshala and McLeodganj attract tourists of all ages from across the globe.",
-	},
-	{
-		place: "Mussoorie and Dehradun, Uttarakhand",
-		dp:
-			"https://www.fabhotels.com/blog/wp-content/uploads/2019/09/Mussoorie1.jpg",
-		desc:
-			"The tree-covered hills and valleys of Dehradun and Mussoorie have attracted families and couples since the 1800s. Offering stunning views of Dehradun on one side and snow-capped Himalayan peaks on the other, the British rightly called Mussoorie, The Queen of Hill Stations. Located merely 33 km apart, these hills are also popular with trekkers. In addition to their natural beauty, Dehradun and Mussoorie boast of a number of institutions, churches, and summer palaces from the British-raj era.",
-	},
-	{
-		place: "Mussoorie and Dehradun, Uttarakhand",
-		dp:
-			"https://www.fabhotels.com/blog/wp-content/uploads/2019/09/Mussoorie1.jpg",
-		desc:
-			"The tree-covered hills and valleys of Dehradun and Mussoorie have attracted families and couples since the 1800s. Offering stunning views of Dehradun on one side and snow-capped Himalayan peaks on the other, the British rightly called Mussoorie, The Queen of Hill Stations. Located merely 33 km apart, these hills are also popular with trekkers. In addition to their natural beauty, Dehradun and Mussoorie boast of a number of institutions, churches, and summer palaces from the British-raj era.",
-	},
+  {
+    place: "Jammu & Kashmir",
+    dp: kashmirbg,
+    desc: "Jammu & Kashmir",
+    attractions: [
+      {
+        attraction: "Shrinagar",
+        dp: Shrinagar,
+        desc:
+          "One of the seven wonders of the world, Taj Mahal is located on the banks of River Yamuna in Agra. It was built by the Mughal Emperor Shah Jahan as a memorial for his third wife, Mumtaz Mahal. It also houses the tomb of Shah Jahan himself. Constructed entirely out of white marble in the 17th century, Taj Mahal is one of the most beautiful monuments in the world. Every year visitors numbering more than the entire population of Agra pass through the magnificent gates to catch a glimpse of this breathtaking monument!",
+      },
+      {
+        attraction: "Gulmarg",
+        dp: Gulmarg,
+        desc:
+          "Located at a distance of 40km from Agra, Fatehpur Sikri is a town in the Agra district and a famous tourist attraction. A city predominantly made of red sandstone, Fatehpur Sikri was founded in 1571 century by Mughal Emperor Akbar. It is essentially a fortified city built by the king and had been the capital of his Empire for fifteen years. Now a UNESCO world heritage site, it is home to Jodha Bai's Palace, Jama Mosque, Buland Darwaza, and Tomb of Salim Chisti among many other famous monuments. Derived from Arab and central Asian tent encampments, the Imperial Complex is a work of pavilions arranged in formal geometry on a piece of land. ",
+      },
+      {
+        attraction: "Pahalgam",
+        dp: Pahalgam,
+        desc:
+          "One of the seven wonders of the world, Taj Mahal is located on the banks of River Yamuna in Agra. It was built by the Mughal Emperor Shah Jahan as a memorial for his third wife, Mumtaz Mahal. It also houses the tomb of Shah Jahan himself. Constructed entirely out of white marble in the 17th century, Taj Mahal is one of the most beautiful monuments in the world. Every year visitors numbering more than the entire population of Agra pass through the magnificent gates to catch a glimpse of this breathtaking monument!",
+      },
+    ],
+  },
+  {
+    place: "Agra, Uttar Pradesh",
+    dp: agrabg,
+    desc:
+      "Agra, home to the famous Taj Mahal, counts among the top places to visit in North India and for all the right reasons. The historical city in Uttar Pradesh is a medley of grand monuments, exceptional architecture, and mesmerizing sights and sounds. It boasts of three UNESCO World Heritage sites, several enticing gardens, vibrant local markets, emporiums, and lip-smacking street food. Agra is also part of the famous Golden-Triangle circuit",
+    attractions: [
+      {
+        attraction: "Taj Mahal",
+        dp: tajMahal,
+        desc:
+          "One of the seven wonders of the world, Taj Mahal is located on the banks of River Yamuna in Agra. It was built by the Mughal Emperor Shah Jahan as a memorial for his third wife, Mumtaz Mahal. It also houses the tomb of Shah Jahan himself. Constructed entirely out of white marble in the 17th century, Taj Mahal is one of the most beautiful monuments in the world. Every year visitors numbering more than the entire population of Agra pass through the magnificent gates to catch a glimpse of this breathtaking monument!",
+      },
+      {
+        attraction: "Fatehpur Sikri",
+        dp: fatahpur,
+        desc:
+          "Located at a distance of 40km from Agra, Fatehpur Sikri is a town in the Agra district and a famous tourist attraction. A city predominantly made of red sandstone, Fatehpur Sikri was founded in 1571 century by Mughal Emperor Akbar. It is essentially a fortified city built by the king and had been the capital of his Empire for fifteen years. Now a UNESCO world heritage site, it is home to Jodha Bai's Palace, Jama Mosque, Buland Darwaza, and Tomb of Salim Chisti among many other famous monuments. Derived from Arab and central Asian tent encampments, the Imperial Complex is a work of pavilions arranged in formal geometry on a piece of land. ",
+      },
+      {
+        attraction: "Agra",
+        dp: agraFort,
+        desc:
+          "One of the seven wonders of the world, Taj Mahal is located on the banks of River Yamuna in Agra. It was built by the Mughal Emperor Shah Jahan as a memorial for his third wife, Mumtaz Mahal. It also houses the tomb of Shah Jahan himself. Constructed entirely out of white marble in the 17th century, Taj Mahal is one of the most beautiful monuments in the world. Every year visitors numbering more than the entire population of Agra pass through the magnificent gates to catch a glimpse of this breathtaking monument!",
+      },
+    ],
+  },
+  {
+    place: "Shimla, Himachal Pradesh",
+    dp: "https://www.fabhotels.com/blog/wp-content/uploads/2019/04/Shimla1.jpg",
+    desc:
+      "Shimla, the picturesque capital city of Himachal Pradesh, is located at the foothills of the Himalayas and offers a unique travel experience with its snow-capped mountains, forests, lush green valleys, placid lakes, and pleasant climatic conditions. Apart from these natural attractions, the region also boasts of several man-made wonders including religious places, historical structures, museums, and the lively Mall Road.",
+  },
 ];
 
-export { north };
+const south = [];
+
+export { north, south };
--- src/components/North.jsx
+++ src/components/North.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import "./styles.css"
+import React from "react";
+import "./styles.css";
 import { motion, AnimatePresence } from "framer-motion";
 import { NavLink } from "react-router-dom";
-import styled from 'styled-components';
-import { north } from './Database';
+import styled, { isStyledComponent } from "styled-components";
+import { north } from "./Database";
 
 const Container = styled.div`
   display: flex;
@@ -22,7 +22,7 @@ const Item = styled.div`
   scroll-snap-align: start;
   height: 100vh;
   width: 100%;
-  background-image: url(${p => p.s});
+  background-image: url(${(p) => p.s});
   background-color: #eac;
   background-position: center;
   background-repeat: no-repeat;
@@ -35,59 +35,79 @@ const About = styled.div`
   justify-content: center;
   align-items: center;
   color: #fff;
-  font-family: 'Kaushan Script';
+  font-family: "Kaushan Script";
   /* backdrop-filter: blur(2px); */
   padding: 20px;
   font-size: 22px;
   background-color: rgba(0, 0, 0, 0.4);
 `;
 
-const Place = ({ s, d }) => {
-    return (
-        <Block>
-            <Item s={s}>
-                <About>{d}</About>
-            </Item>
-        </Block>
-    );
+const Place = ({ s, d, attractions }) => {
+  return (
+    <Block>
+      <Item s={s}>
+        <About>{d}</About>
+      </Item>
+    </Block>
+  );
 };
 
-
 const North = () => {
-    const pageVariant = {
-        initial: {
-            opacity: 0, y: "-100%",
-            scale: 1.2
-        },
-        in: {
-            opacity: 1, y: "0",
-            scale: 1
-        },
-        out: {
-            opacity: 0, y: "-100%",
-            scale: 1.2
-        },
+  console.log(north);
+  const pageVariant = {
+    initial: {
+      opacity: 0,
+      y: "-100%",
+      scale: 1.2,
+    },
+    in: {
+      opacity: 1,
+      y: "0",
+      scale: 1,
+    },
+    out: {
+      opacity: 0,
+      y: "-100%",
+      scale: 1.2,
+    },
+  };
 
-    };
+  const pageTransition = {
+    type: "tween",
+    ease: "easeOut",
+    duration: 0.5,
+  };
 
-    const pageTransition = {
-        type: "tween",
-        ease: "easeOut",
-        duration: .5
-    };
-
-    return (<><NavLink className="arrow down" to="/" exact>
+  return (
+    <>
+      <NavLink className="arrow down" to="/" exact>
         🡫
-    </NavLink>
-        <motion.div style={{ position: "absolute" }} variants={pageVariant} transition={pageTransition} initial="initial" animate="in" exit="out" className="bg north" id="North">
-            <h1 className="region northh1">North India</h1>
-            <Container>
-                {north.map((city, index) => (
-                    <Place key={index} city={city.place} s={city.dp} d={city.desc} />
-                ))}
-            </Container>
-        </motion.div></>
-    );
+      </NavLink>
+      <motion.div
+        style={{ position: "absolute" }}
+        variants={pageVariant}
+        transition={pageTransition}
+        initial="initial"
+        animate="in"
+        exit="out"
+        className="bg north"
+        id="North"
+      >
+        <h1 className="region northh1">North India</h1>
+        <Container>
+          {north.map((city, index) => (
+            <Place
+              key={index}
+              city={city.place}
+              s={city.dp}
+              d={city.desc}
+              attractions={city.attractions}
+            />
+          ))}
+        </Container>
+      </motion.div>
+    </>
+  );
 };
 
 export default North;
